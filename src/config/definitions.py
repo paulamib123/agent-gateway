0b1ec22e9fd3153401a62b655ea648a54f6d9875
paulamib123 <paulamixbhattacharya@gmail.com>
2023-01-02 15:07:47 +0530

    changed timezone of logs to ist

--- src/config/definitions.py
+++ src/config/definitions.py
@@ -5,5 +5,7 @@ ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 LOG_DIR = os.path.realpath(os.path.join(ROOT_DIR, '..')) + "/logs"
 
 
-
-logging.basicConfig(filename=LOG_DIR + "/api.log", level=logging.INFO, format="%(asctime)s:%(levelname)s: %(message)s")
+logging.basicConfig(filename=LOG_DIR + "/api.log", 
+                    level=logging.INFO, 
+                    format="%(asctime)s:%(levelname)s: %(message)s",
+                    datefmt='%Y-%m-%d %H:%M:%S')
